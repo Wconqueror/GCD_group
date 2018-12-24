d1c37b50fc8009db3e18a10923bb5bf9a1878cf5
hgf <hgf@hgfdeMacBook-Pro.local>
2018-12-24 17:30:03 +0800

    创建分支

---
+++ GCD/MyView.h
@@ -0,0 +1,17 @@
+//
+//  MyView.h
+//  GCD
+//
+//  Created by hgf on 2018/12/24.
+//  Copyright © 2018 com.youqii.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GCD/MyView.m
@@ -0,0 +1,21 @@
+//
+//  MyView.m
+//  GCD
+//
+//  Created by hgf on 2018/12/24.
+//  Copyright © 2018 com.youqii.com. All rights reserved.
+//
+
+#import "MyView.h"
+
+@implementation MyView
+
+-(instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
